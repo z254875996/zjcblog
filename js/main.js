58dddfcd3f972165ceee89ac8519ce69d9541023
Your Name <you@example.com>
2016-11-20 15:14:23 +0800

    添加了图标/隐藏box

---
+++ js/main.js
@@ -0,0 +1,23 @@
+
+
+
+/*$(document).ready(function(){
+  $(".header-nav-wenzhang").hover(function(){
+    $(".header-nav-Hidden-wenzhang").animate({
+      height:'toggle'
+    });
+  });
+});*/
+
+$(document).ready(function(){
+  $(".icon-sort").click(function(){
+    $(".Hidden_box").animate({
+      height:'toggle'
+    });
+  });
+});
+
+
+
+
+

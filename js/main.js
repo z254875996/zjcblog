acb13d0fc6f838695f886f140b6552415397bec7
Your Name <you@example.com>
2016-12-12 16:46:05 +0800

    备案完成后上传

--- js/main.js
+++ js/main.js
@@ -1,14 +1,5 @@
 
 
-
-/*$(document).ready(function(){
-  $(".header-nav-wenzhang").hover(function(){
-    $(".header-nav-Hidden-wenzhang").animate({
-      height:'toggle'
-    });
-  });
-});*/
-
 $(document).ready(function(){
   $(".icon-sort").click(function(){
     $(".Hidden_box").animate({
@@ -17,7 +8,17 @@ $(document).ready(function(){
   });
 });
 
+jQuery(".nav").slide({ 
+				type:"menu", //效果类型
+				titCell:".m", // 鼠标触发对象
+				targetCell:".sub", // 效果对象，必须被titCell包含
+				effect:"slideDown",//下拉效果
+				delayTime:400, // 效果时间
+				triggerTime:0, //鼠标延迟触发时间
+				returnDefault:true  //返回默认状态
+			});
 
+             
 
 
 
